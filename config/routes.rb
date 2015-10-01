1ea743393268ecfa36ecdc1f54c92bcbbd7ccd87
YoanDieu <yoan.dieu@gmail.com>
2015-10-01 21:04:52 +0200

    fix merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :posts
   get 'public/wall'
 
   #SECOND OPTION
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20151001153818) do
 
+  create_table "posts", force: :cascade do |t|
+    t.string   "message"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
