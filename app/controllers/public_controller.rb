89d785dd8c34f82af9afb533217f4e1a91bb8c55
YoanDieu <yoan.dieu@gmail.com>
2015-10-01 22:06:21 +0200

    added form for posting on user's self wall, setted controller, displayed the user's posts on the wall

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,4 +1,18 @@
 class PublicController < ApplicationController
   def wall
+    @user = current_user
+    @posts = @user.posts
+  end
+
+  def publish
+    @user = current_user
+    @post = Post.new message: params[:post][:message], user_id: @user.id
+
+    if @post.save
+      render 'wall'
+    else
+      render 'wall'
+    end
+
   end
 end
